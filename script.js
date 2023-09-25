304e10442e13b6a57b8a894bee1ec16e65d600df
Jayden338 <Jayden.marks338@outlook.com>
2023-09-25 15:15:42 +0930

    Added prompts

--- script.js
+++ script.js
@@ -5,10 +5,36 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"]; 
+var numeric = ["0123456789"];
+var specialCharacters = ["!@#$%^&*()"]; 
 
-}
+} 
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword(){ 
+} 
+
+var userChoice = window.prompt("How would many characters 8 - 128"); 
+if (userChoice < 7)  {
+ window.alert ("Must be more than 7 characters")
+}
+ if (userChoice > 128) {
+  window.alert ("Must be less than 128 characters")
+} 
+
+
+
+
+
+window.confirm("Include Lowercase")
+window.confirm("Include Uppercase") 
+window.confirm("Include Numeric") 
+window.confirm("Include Special Characters") 
