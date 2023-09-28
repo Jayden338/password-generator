a7cf3a279f1e2c6616c8bc58d97dc53bd6dcc345
Jayden338 <Jayden.marks338@outlook.com>
2023-09-28 23:58:11 +0930

    Final try

--- script.js
+++ script.js
@@ -10,34 +10,34 @@
 
 // Function to prompt user for password options
 function getPasswordOptions() { 
-  
+
 // Variable to store length of password from user input
-var characterLength = prompt("How many characters 8 - 128");
+var characterLength = prompt("How Many Characters Between 8 - 128?");
 characterLength = parseInt(characterLength);
 
 // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
 if (characterLength < 8) { 
-window.alert ("Must have at least 8 characters"); 
+window.alert ("Must Have At Least 8 Characters!"); 
  return prompt;
 }
   
 // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
 if (characterLength > 128) {
-window.alert ("Must be less than 128 characters"); 
+window.alert ("Must Be Less Than 128 Characters!"); 
 return prompt;
 } 
 
 // Variable to store boolean regarding the inclusion of special characters
-  var specialCharactersUser = confirm("Include Special Characters?")
+  var specialCharactersUser = confirm("Include Special Characters In Password?")
 
 // Variable to store boolean regarding the inclusion of numeric characters
-  var numericUser = confirm("Include numeric?")
+  var numericUser = confirm("Include Numeric In Password?")
  
 // Variable to store boolean regarding the inclusion of lowercase characters
-  var lowerCaseUser = confirm("Include lower case?")
+  var lowerCaseUser = confirm("Include Lower Case In Password?")
 
 // Variable to store boolean regarding the inclusion of uppercase characters
-  var upperCaseUser = confirm("Include upper case?")
+  var upperCaseUser = confirm("Include Upper Case In Password?")
   
 // Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
 if (specialCharactersUser) {
@@ -45,7 +45,7 @@ if (specialCharactersUser) {
 } else if (lowerCaseUser) {
 } else if (upperCaseUser) {
 } else {
-  window.alert ("Must confirm at least one box") 
+  window.alert ("Must Confirm At Least One Box!") 
   return prompt;
 } 
 
@@ -62,12 +62,12 @@ if (specialCharactersUser) {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  var randomLetter = Math.floor(Math.random() * userInput.length);
+  var randomLetter = Math.floor(Math.random() * userInput.characterLength);
  }
  
   // Function to generate password with user input
 function generatePassword() {
-  var options = getPasswordOptions();
+   passwordOptions = getPasswordOptions(); 
   // Conditional statement that adds array of numeric characters into array of possible characters based on user input
   // Push new random special character to guaranteedCharacters
  
@@ -89,8 +89,7 @@ function generatePassword() {
   if (confirm.upperCaseUser) { 
     userInput = userInput.concat(upperCase);
   } 
-console.log(userInput)
-
+return 
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
