55ac0e251fa890d5ed1f46e3752374d998ed32a9
Jayden338 <Jayden.marks338@outlook.com>
2023-09-28 17:02:32 +0930

    Fixed some code up for JS with comment hints

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',]; 
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var userInput = [];
+var characterLength = [];
+
+// Function to prompt user for password options
+function getPasswordOptions() { 
+  
+// Variable to store length of password from user input
+var characterLength = prompt("How many characters 8 - 128");
+characterLength = parseInt(characterLength);
+
+// Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+if (characterLength < 8) { 
+window.alert ("Must have at least 8 characters"); 
+ return prompt;
+}
+  
+// Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+if (characterLength > 128) {
+window.alert ("Must be less than 128 characters"); 
+return prompt;
+} 
+
+
+// Function to generate password with user input
+function generatePassword() {
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
-var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lowercase = ["abcdefghijklmnopqrstuvwxyz"]; 
-var numeric = ["0123456789"];
-var specialCharacters = ["!@#$%^&*()"]; 
+var password = generatePassword();
+var passwordText = document.querySelector('#password');
 
-} 
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-function generatePassword(){ 
-} 
-
-var userChoice = window.prompt("How would many characters 8 - 128"); 
-if (userChoice < 7)  {
- window.alert ("Must be more than 7 characters")
+passwordText.value = password;
 }
- if (userChoice > 128) {
-  window.alert ("Must be less than 128 characters")
-} 
-
-
-
-
-
-window.confirm("Include Lowercase")
-window.confirm("Include Uppercase") 
-window.confirm("Include Numeric") 
-window.confirm("Include Special Characters") 
+}
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);  
+}
